6b297ed7fe87022c4920c43515237d41262e5518
saikrishna <potharaboinasaikrishna1@gmail.com>
2026-09-20 12:55:43 +0530

    chnages

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,23 +14,4 @@ const api = axios.create({
     }
 })
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('neolit_token')
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-})
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if ([401, 403].includes(error.response?.status) && localStorage.getItem('neolit_token')) {
-            localStorage.removeItem('neolit_token')
-            window.location.assign('/login')
-        }
-        return Promise.reject(error)
-    },
-)
-
 export default api
